c7aebe611e11476a72743126a5d16ee21f3e34f1
jashshah1997 <jashshah37@yahoo.com>
2022-04-22 00:29:32 +0200

    Add main menu

--- Assets/Scripts/UIScripts/MainMenuCanvas.cs
+++ Assets/Scripts/UIScripts/MainMenuCanvas.cs
@@ -5,9 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuCanvas : MonoBehaviour
 {
+    private GameObject m_startPanel;
+    private GameObject m_instructionsPanel;
+
+    private void Awake()
+    {
+        m_startPanel = GameObject.Find("StartPanel");
+        m_instructionsPanel = GameObject.Find("InstructionsPanel");
+        m_instructionsPanel.SetActive(false);
+    }
 
     public void LoadGameScene()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void ToggleInstructions(bool value)
+    {
+        m_startPanel.SetActive(!value);
+        m_instructionsPanel.SetActive(value);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
